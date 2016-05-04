4adec21ba10c3a9c778609e49d1e7b57c7f56d49
hirenshah7390 <hirenshah7390@hotmail.com>
2016-05-03 22:37:47 -0500

    sleep analyzer

--- SparkMachineLearnin/src/main/scala/com/umkc/sparkML/SleepAnalyzer.scala
+++ SparkMachineLearnin/src/main/scala/com/umkc/sparkML/SleepAnalyzer.scala
@@ -1,15 +1,21 @@
 package com.umkc.sparkML
 
+import java.io.{File, PrintWriter}
+
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark._
 import org.apache.spark.rdd._
 import org.apache.spark.SparkContext._
 import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{DoubleType, StringType, StructField}
 
 /**
-  * Created by hhstm4 on 5/3/2016.
+  * Created by Hiren Shah on 5/3/2016.
   */
-object SleepAnalyzer {
+object SleepAnalyzer   {
   def main(args: Array[String]) {
 
     System.setProperty("hadoop.home.dir", "c:\\winutils")
@@ -21,36 +27,57 @@ object SleepAnalyzer {
       .set("spark.executor.memory", "2g").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
+    //mysql connection
+    val databaseUsername = "heartfit_root"
+    val databasePassword = "heart123"
+    val databaseConnectionUrl = "jdbc:mysql://srv70.hosting24.com:3306/heartfit_123?user=" + databaseUsername + "&password=" + databasePassword
+
+
     val dataset = sc.textFile("ml-100k/sample_fpgrowth.txt")
       .map(line => {
         val fields = line.split(" ")
-        (fields(0).toInt, fields(1).toInt, fields(2).toInt, fields(3).toInt)
+        (fields(0).toDouble, fields(1).toDouble, fields(2).toDouble, fields(3), fields(4).toInt, fields(5).toInt)
       })
 
-    val count = dataset.count()
+    val pairs = dataset.cartesian(dataset)
 
-    var i = 0
-    for(i <- 1 to count.toInt){
-      
-        println(i)
+    val filter = pairs.filter(f => (f._1._1 != f._2._1 || f._1._2 != f._2._2 || f._1._3 != f._2._3) && f._1._4 == f._2._4 && f._1._5 == f._2._5 && f._1._6 == f._2._6)
 
-    }
+    //filter.collect().foreach(println)
 
+    val result = filter.map(r => {
+      val key = (r._1._1,r._1._2,r._1._3,r._1._4,r._1._5,r._1._6)
+      val value = 1;
 
-    // dataset.foreach()
+      (key, value)
+    }).reduceByKey(_ + _)
 
-    //  val duplicate = dataset.keyBy(tup => tup._1)
-    //
-    //  val matrixPairs =
-    //    dataset
-    //      .keyBy(tup => tup._1)
-    //      .join(duplicate)
-    //      .filter(f => (f._2._1._2 != f._2._2._2))
+   // result.collect().foreach(println)
 
 
+    val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
+    import sqlContext.implicits._
+
+
+   val df =  result.map {
+     case (k, v) => acc(k._1, k._2, k._3, k._4, k._5, k._6,v) }.toDF()
+
+
+    val accl = Window.partitionBy('d,'t,'h,'cnt).orderBy('d.desc)
+
+    val ranked = df.withColumn("rank", row_number().over(accl))
+    ranked.registerTempTable("resultitems")
+
+    val finalresults = sqlContext.sql("SELECT x,y,z,d,t,h,cnt FROM resultitems where rank = 1")
+
+    //finalresults.collect().foreach(println)
+
+    finalresults.insertIntoJDBC(databaseConnectionUrl,"output_accelerometer",false)
   }
 
+  case class acc(x: Double,y: Double, z: Double, d: String, t: Int, h: Int, cnt: Int)
 
-
+  case class StructType(value: Any)
 
 }
+
