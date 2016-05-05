f40a2176aa303dd2df0081dcfed36dbd2b3e2f0d
hirenshah7390 <hirenshah7390@hotmail.com>
2016-05-04 22:05:41 -0500

    ppt added

--- SparkMachineLearnin/src/main/scala/com/umkc/sparkML/SimpleFPGrowth.scala
+++ SparkMachineLearnin/src/main/scala/com/umkc/sparkML/SimpleFPGrowth.scala
@@ -3,10 +3,17 @@ package com.umkc.sparkML
 /**
   * Created by hhstm4 on 4/6/2016.
   */
+
+import java.io.{File, PrintWriter}
+import java.sql.DriverManager
+import java.text.SimpleDateFormat
+
 import org.apache.spark.{SparkConf, SparkContext}
 // $example on$
-import org.apache.spark.mllib.fpm.FPGrowth
-import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.fpm.{AssociationRules, FPGrowth}
+import org.apache.spark.rdd.{JdbcRDD, RDD}
+import java.util.{Date, Calendar}
+import org.apache.spark.mllib.fpm.FPGrowth.FreqItemset
 // $example off$
 
 object SimpleFPGrowth {
@@ -14,33 +21,84 @@ object SimpleFPGrowth {
   def main(args: Array[String]) {
     System.setProperty("hadoop.home.dir","c:\\winutils")
 
-    val conf =new SparkConf().setMaster("local[*]").setAppName("SparkNaiveBayes").set("spark.driver.memory", "3g").set("spark.executor.memory", "3g")
+    val conf = new SparkConf()
+      .setAppName("pattern")
+      .set("spark.executor.memory", "2g").setMaster("local[*]")
     val sc = new SparkContext(conf)
    // val conf = new SparkConf().setAppName("SimpleFPGrowth")
     //val sc = new SparkContext(conf)
 
+    //mysql connection
+    val databaseUsername = "heartfit_root"
+    val databasePassword = "heart123"
+    val databaseConnectionUrl = "jdbc:mysql://srv70.hosting24.com:3306/heartfit_123?user=" + databaseUsername + "&password=" + databasePassword
+
+    val date = new Date();
+    val readFormat = new SimpleDateFormat( "MMM dd, yyyy hh:mm:ss aa").format(date);
+    val writeFormat = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss").format(readFormat)
+
+    val modifiedDate= new SimpleDateFormat("yyyy-MM-dd").format(date)
+    val modifiedhour = new SimpleDateFormat("hh").format(writeFormat)
+
+    println(modifiedDate + " " + modifiedhour )
+
+    val rdd = new JdbcRDD( sc, () =>
+      DriverManager.getConnection(databaseConnectionUrl,databaseUsername,databasePassword) ,
+      "select heart_rate,date from heart_rate limit ?, ?",
+      1, 5000, 2, r => r.getString("heart_rate") + " " + r.getString("date") )
+
     // $example on$
-    val data = sc.textFile("ml-100k/sample_fpgrowth.txt")
+    val data = sc.textFile("sample_fpgrowth.txt")
 
-    val transactions: RDD[Array[String]] = data.map(s => s.trim.split(' '))
+    val transactions: RDD[Array[String]] = rdd.map(s => s.trim.split(" "))
 
-    val fpg = new FPGrowth()
-      .setMinSupport(0.2)
-      .setNumPartitions(10)
-    val model = fpg.run(transactions)
+    val freqItemsets = transactions
+      .flatMap(xs =>
+        (xs.combinations(1) ++ xs.combinations(2)).map(x => (x.toList, 1L))
+      )
+      .reduceByKey(_ + _)
+      .map{case (xs, cnt) => new FreqItemset(xs.toArray, cnt)}
 
-    model.freqItemsets.collect().foreach { itemset =>
-      println(itemset.items.mkString("[", ",", "]") + ", " + itemset.freq)
-    }
 
-//    val minConfidence = 0.8
-//    model.generateAssociationRules(minConfidence).collect().foreach { rule =>
-//      println(
-//        rule.antecedent.mkString("[", ",", "]")
-//          + " => " + rule.consequent .mkString("[", ",", "]")
-//          + ", " + rule.confidence)
-//    }
-    // $example off$
+    val rt = freqItemsets.map(itemset =>
+      {
+        val key =itemset.items.mkString("[", ",", "]")
+        val value =   itemset.freq
+        (key,value)
+
+      })
+      .filter(r => r._1.length() < 5)
+
+
+  }
+  case class ht(out_hr: Int,out_num: Int, out_date: String , out_hour: Int)
+
+  def GetHour(hour : Int) = {
+
+    val result = if(hour == 1)
+      13
+    else if(hour == 2)
+      14
+    else if(hour == 3)
+      15
+    else if(hour == 4)
+      16
+    else if(hour == 5)
+      17
+    else if(hour == 6)
+      18
+    else if(hour == 7)
+      19
+    else if(hour == 8)
+      20
+    else if(hour == 9)
+      21
+    else if(hour == 10)
+      12
+    else if(hour == 11)
+      23
+
+    result
   }
 }
 // scalastyle:on println
--- SparkMachineLearnin/src/main/scala/com/umkc/sparkML/SleepAnalyzer.scala
+++ SparkMachineLearnin/src/main/scala/com/umkc/sparkML/SleepAnalyzer.scala
@@ -1,6 +1,7 @@
 package com.umkc.sparkML
 
 import java.io.{File, PrintWriter}
+import java.sql.DriverManager
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark._
@@ -32,27 +33,30 @@ object SleepAnalyzer   {
     val databasePassword = "heart123"
     val databaseConnectionUrl = "jdbc:mysql://srv70.hosting24.com:3306/heartfit_123?user=" + databaseUsername + "&password=" + databasePassword
 
+    val rdd = new JdbcRDD( sc, () =>
+      DriverManager.getConnection(databaseConnectionUrl,databaseUsername,databasePassword) ,
+      "select format(rate_x,0) rate_x,format(rate_y,0) rate_y,format(rate_z,0) rate_z,read_date,read_hour,read_min,ID from accelerometer limit ?, ?",
+      1, 50, 2, r => r.getString("rate_x") + " " + r.getString("rate_y") + " " + r.getString("rate_z") + " " + r.getString("read_date") + " " + r.getString("read_hour") + " " + r.getString("read_min") + " " + r.getString("ID"))
 
-    val dataset = sc.textFile("ml-100k/sample_fpgrowth.txt")
+    val dataset = rdd
       .map(line => {
         val fields = line.split(" ")
-        (fields(0).toDouble, fields(1).toDouble, fields(2).toDouble, fields(3), fields(4).toInt, fields(5).toInt)
+        (fields(0).toInt, fields(1).toInt, fields(2).toInt, fields(3), fields(4).toInt, fields(5).toInt, fields(6))
       })
 
     val pairs = dataset.cartesian(dataset)
 
-    val filter = pairs.filter(f => (f._1._1 != f._2._1 || f._1._2 != f._2._2 || f._1._3 != f._2._3) && f._1._4 == f._2._4 && f._1._5 == f._2._5 && f._1._6 == f._2._6)
+    val filter = pairs.filter(f => (f._1._1 != f._2._1 || f._1._2 != f._2._2 || f._1._3 != f._2._3) && f._1._4 == f._2._4 && f._1._5 == f._2._5 && f._1._6 == f._2._6 && f._1._7 != f._2._7)
 
-    //filter.collect().foreach(println)
+   // filter.collect().foreach(println)
 
     val result = filter.map(r => {
-      val key = (r._1._1,r._1._2,r._1._3,r._1._4,r._1._5,r._1._6)
+      val key = (r._1._1,r._1._2,r._1._3,r._1._4,r._1._5,r._1._6,r._1._7)
       val value = 1;
-
       (key, value)
     }).reduceByKey(_ + _)
 
-   // result.collect().foreach(println)
+    //result.collect().foreach(println)
 
 
     val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
@@ -60,22 +64,52 @@ object SleepAnalyzer   {
 
 
    val df =  result.map {
-     case (k, v) => acc(k._1, k._2, k._3, k._4, k._5, k._6,v) }
+     case (k, v) => acc(k._1, k._2, k._3, k._4, k._5, k._6,v, k._7.toInt) }
      .toDF()
 
-
-    val accl = Window.partitionBy('out_date,'out_hour,'out_min,'out_count).orderBy('out_date.desc)
+    val accl = Window.partitionBy('out_date,'out_hour,'out_min).orderBy('out_count.asc)
 
     val ranked = df.withColumn("rank", row_number().over(accl))
     ranked.registerTempTable("resultitems")
 
     val finalresults = sqlContext.sql("SELECT out_x,out_y,out_z,out_date,out_hour,out_min,out_count FROM resultitems where rank = 1")
 
-    finalresults.insertIntoJDBC(databaseConnectionUrl,"output_accelerometer",true)
+    finalresults.collect().foreach(println)
+
+   //finalresults.insertIntoJDBC(databaseConnectionUrl,"output_accelerometer",true)
 
   }
 
-  case class acc(out_x: Double,out_y: Double, out_z: Double, out_date: String, out_hour: Int, out_min: Int, out_count: Int)
+  def GetHour(hour : Int) = {
+
+    val result = if(hour == 1)
+                    13
+    else if(hour == 2)
+       14
+    else if(hour == 3)
+      15
+    else if(hour == 4)
+      16
+    else if(hour == 5)
+      17
+    else if(hour == 6)
+      18
+    else if(hour == 7)
+      19
+    else if(hour == 8)
+      20
+    else if(hour == 9)
+      21
+    else if(hour == 10)
+      12
+    else if(hour == 11)
+      23
+
+    result
+  }
+
+
+  case class acc(out_x: Int,out_y: Int, out_z: Int, out_date: String, out_hour: Int, out_min: Int, out_count: Int, ID : Int)
 
 }
 
